f36a25300752b44f2aa92944e23c7598d93cd789
Hamilton Chapman <hamchapman@gmail.com>
2013-11-25 18:38:02 +0000

    Styled and tested. My work here is done

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,4 @@
 class Photo < ActiveRecord::Base
-  has_attached_file :image, styles: { large: "500x500>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>" }, default_url: "/images/:style/missing.png"
   belongs_to :user
 end
